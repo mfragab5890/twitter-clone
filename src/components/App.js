12045a01c0f4063838f9ea8f5d6623a4bc5da4db
ghelpo <m.f.ragab5890@gmail.com>
2021-09-27 19:30:29 +0200

    app-layout-update
    updating the initial design of the app

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,25 @@ class App extends Component {
   render() {
     return (
       <div>
-        Starter Code
+        <div className="ui secondary pointing menu">
+          <a className="active item">
+            Home
+          </a>
+          <a className="item">
+            <i class="plus circle green icon"></i>New Tweet
+          </a>
+          <div class="right menu">
+            <div class="item">
+              <div class="ui transparent icon input">
+                <input type="text" placeholder="Search by users..." />
+                <i class="search link icon"></i>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="ui segment">
+          <p></p>
+        </div>
       </div>
     )
   }
