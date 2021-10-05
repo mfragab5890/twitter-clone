85f77bf8bde2c397f88fc0aa807cb87431add738
ghelpo <m.f.ragab5890@gmail.com>
2021-10-05 13:54:06 +0200

    create-actions
    created initial actions for our app

---
+++ src/actions/tweets.js
@@ -0,0 +1,10 @@
+//handle tweets action creator
+
+export const RECEIVE_TWEETS = 'RECEIVE_TWEETS'
+
+export const receiveTweets = (tweets) => {
+  return {
+    type : RECEIVE_TWEETS,
+    tweets,
+  };
+}
