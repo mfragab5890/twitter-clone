3cb9a88d6eb686d2dc406f0419de41e5ee3db9b6
ghelpo <m.f.ragab5890@gmail.com>
2021-10-14 10:14:25 +0200

    Update Dashboard.js

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import {Route} from 'react-router-dom'
 import Tweet from './Tweet'
+import NewTweet from './NewTweet'
 
 class Dashboard extends React.Component {
 
@@ -12,12 +14,24 @@ class Dashboard extends React.Component {
     const {tweetsIds} = this.props
 
     return (
-      <div className="ui centered container comments">
-        {
-          tweetsIds.map((tweet) => <Tweet key = {tweet} tweetId = {tweet}/> )
-        }
+      <div>
+
+        <Route exact path='/new-tweet' render ={() => (
+            <NewTweet />
+          )}
+        />
+
+      <Route exact path = '/' render ={() => (
+          <div className="ui centered container comments">
+            {
+              tweetsIds.map((tweetId) => <Tweet key = {tweetId} tweetId = {tweetId}/> )
+            }
+          </div>
+          )}
+        />
 
       </div>
+
     );
   };
 }
