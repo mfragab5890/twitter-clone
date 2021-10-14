e9290e7524f6f48513d63930e799f984e3b3d121
ghelpo <m.f.ragab5890@gmail.com>
2021-10-14 10:15:02 +0200

    Update tweets.js
    handle add new tweet

--- src/actions/tweets.js
+++ src/actions/tweets.js
@@ -1,8 +1,19 @@
+import { saveTweet } from '../utils/api'
+import { showLoading, hideLoading } from 'react-redux-loading'
+
 //handle tweets action creator
 import { saveLikeToggle } from '../utils/api'
 
 export const RECEIVE_TWEETS = 'RECEIVE_TWEETS'
 export const TOGGLE_TWEET = 'TOGGLE_TWEET'
+export const ADD_TWEET = 'ADD_TWEET'
+
+export const addTweet = (tweet) => {
+  return {
+    type : ADD_TWEET,
+    tweet,
+  };
+}
 
 export const receiveTweets = (tweets) => {
   return {
@@ -26,5 +37,20 @@ export const handleTweetLikeToggle = (info) => (dispatch) => {
     dispatch(toggleTweet(info))
     console.warn('Error in handleTweetLikeToggle' ,e);
   })
+}
 
+export const handleAddTweet = (text, replyingTo) => (dispatch, getState) =>{
+  dispatch(showLoading)
+  const { authedUser } = getState()
+  return saveTweet({
+    text,
+    author : authedUser,
+    replyingTo,
+    })
+    .then((tweet) => {
+      dispatch(addTweet(tweet))
+    })
+    .then(() => {
+      dispatch(hideLoading())
+    })
 }
