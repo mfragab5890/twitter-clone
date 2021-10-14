974696bd9260d89b1fe9bbe350a4610ce032ae12
ghelpo <m.f.ragab5890@gmail.com>
2021-10-14 10:16:49 +0200

    Update tweets.js
    handle adding new tweet action

--- src/reducers/tweets.js
+++ src/reducers/tweets.js
@@ -1,4 +1,4 @@
-import { RECEIVE_TWEETS, TOGGLE_TWEET } from '../actions/tweets'
+import { RECEIVE_TWEETS, TOGGLE_TWEET, ADD_TWEET } from '../actions/tweets'
 
 const tweets = (state = {}, action) => {
   switch (action.type) {
@@ -17,6 +17,24 @@ const tweets = (state = {}, action) => {
               : state[action.id].likes.concat([action.authedUser])
           }
         };
+        case ADD_TWEET:
+          const {tweet} = action
+          let replyingTo = {}
+
+          if (tweet.replyingTo !== null) {
+            replyingTo = {
+              [tweet.replyingTo]:{
+                ...state[replyingTo],
+                replies: state[tweet.replyingTo].replies.concat([tweet.id])
+              },
+            }
+          }
+
+          return {
+            ...state,
+            [tweet.id] : tweet,
+            ...replyingTo,
+          };
     default:
       return state;
   }
