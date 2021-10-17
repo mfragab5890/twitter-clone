0a7056e9d16f71789cb029d9fb1894b052580857
ghelpo <m.f.ragab5890@gmail.com>
2021-10-17 19:58:40 +0200

    Update shared.js

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,6 +1,6 @@
-import { getInitialData } from '../utils/api'
-import { receiveUsers } from '../actions/users'
-import { receiveTweets } from '../actions/tweets'
+import { getInitialData, saveTweet } from '../utils/api'
+import { receiveUsers, addUserTweet } from '../actions/users'
+import { receiveTweets, addTweet } from '../actions/tweets'
 import { setAuthedUser } from '../actions/authedUser'
 import { showLoading, hideLoading } from 'react-redux-loading'
 
@@ -17,4 +17,22 @@ export const handleInitialData = () => {
         dispatch(hideLoading())
       })
   };
+}
+
+export const handleAddTweet = (text, replyingTo) => (dispatch, getState) =>{
+  dispatch(showLoading)
+  const { authedUser } = getState()
+  return saveTweet({
+    text,
+    author : authedUser,
+    replyingTo,
+    })
+    .then((tweet) => {
+      console.log('tweet is : ', tweet );
+      dispatch(addUserTweet(tweet.author, tweet.id))
+      dispatch(addTweet(tweet))
+    })
+    .then(() => {
+      dispatch(hideLoading())
+    })
 }
