0839a4137b40b7d883277f0bcdc8a4bf43d7707a
ghelpo <m.f.ragab5890@gmail.com>
2021-10-17 19:58:43 +0200

    Update NewTweet.js

--- src/components/NewTweet.js
+++ src/components/NewTweet.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { Redirect } from 'react-router-dom'
-import { handleAddTweet } from '../actions/tweets'
+import { handleAddTweet } from '../actions/shared'
 
 class NewTweet extends React.Component {
   state = {
